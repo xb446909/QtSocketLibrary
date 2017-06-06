600f625483f01b415c3370c020711dd19be50848
xiong <xiong-bin@live.com>
2017-06-06 14:10:23 +0800

    Optimized the tcp server and add tcp client

--- socket/socket.cpp
+++ socket/socket.cpp
@@ -4,37 +4,49 @@
 #include "iniconfig.h"
 #include "tcpserver.h"
 
+typedef struct _tagSocketParam
+{
+    int nType;
+    QObject* pObj;
+    QString szIniPath;
+
+    ~_tagSocketParam()
+    {
+        pObj->deleteLater();
+    }
+}SocketParam, *pSocketParam;
+
 int _argc = 1;
 QSharedPointer<QCoreApplication> app(new QCoreApplication(_argc, nullptr));
 
-QHash<int, QPair<QSharedPointer<QObject>, int>> g_hashSockets;
-QPair<QSharedPointer<QObject>, int> FindSocket(int nId);
+QHash<int, QSharedPointer<SocketParam>> g_hashSockets;
+pSocketParam FindSocket(int nId);
 
-void DeleteTcpServer(QObject* pObj)
-{
-    TcpServer* pServer = (TcpServer*)pObj;
-    pServer->close();
-    pObj->deleteLater();
-}
 
 int InitSocket(int nID, int nType, const char* szIniPath, RecvCallback pCallback)
 {
     qDebug() << __FUNCTION__;
 
-    g_hashSockets.remove(nID);
-
     IniConfig config(szIniPath);
     QString section;
     TcpServer* pServer;
 
+    g_hashSockets.remove(nID);
+    g_hashSockets.insert(nID, QSharedPointer<SocketParam>(new SocketParam()));
+    g_hashSockets[nID]->nType = nType;
+    g_hashSockets[nID]->szIniPath = QString(szIniPath);
+
     switch (nType)
     {
     case TCP_SERVER:
-        g_hashSockets.insert(nID, qMakePair(QSharedPointer<QObject>(new TcpServer(pCallback), DeleteTcpServer), nType));
-        pServer = (TcpServer*)g_hashSockets[nID].first.data();
+        pServer = new TcpServer(pCallback);
+        g_hashSockets[nID]->pObj = pServer;
         section = QString().sprintf("TcpServer%d", nID);
         pServer->listen(QHostAddress(config.GetValue(section, "Address", "127.0.0.1").toString()),
                         config.GetValue(section, "Port", 9999).toInt());
+        break;
+    case TCP_CLIENT:
+        g_hashSockets[nID]->pObj = new QTcpSocket();
         break;
     default:
         break;
@@ -44,21 +56,41 @@ int InitSocket(int nID, int nType, const char* szIniPath, RecvCallback pCallback
 
 int TCPSend(int nID, const char* szSendBuf, const char* szDstIP, int nDstPort)
 {
-    QPair<QSharedPointer<QObject>, int> socket = FindSocket(nID);
+    pSocketParam socket = FindSocket(nID);
     TcpServer* pServer = nullptr;
     QTcpSocket* pSocket = nullptr;
-    if (socket.first.data() != nullptr)
+    QAbstractSocket* pSendSocket;
+    if (socket != nullptr)
     {
-        switch (socket.second) {
+        switch (socket->nType) {
         case TCP_SERVER:
-            pServer = (TcpServer*)socket.first.data();
+            pServer = (TcpServer*)socket->pObj;
             pSocket = pServer->getSocket(szDstIP, nDstPort);
             if (pSocket == nullptr)
                 return -1;
             else
-                pSocket->write(szSendBuf, strlen(szSendBuf) + 1);
+            {
+                if (pSocket->state() == QTcpSocket::ConnectedState)
+                    pSocket->write(szSendBuf, strlen(szSendBuf) + 1);
+                else
+                    qDebug() << "Socket is disconnected";
+            }
+            break;
+        case TCP_CLIENT:
+            pSendSocket = (QAbstractSocket*)socket->pObj;
+            if (pSendSocket == nullptr)
+                return -1;
+            else
+            {
+                if (pSendSocket->state() == QAbstractSocket::ConnectedState)
+                    pSendSocket->write(szSendBuf, strlen(szSendBuf) + 1);
+                else
+                    qDebug() << "Socket is disconnected";
+            }
             break;
         default:
+            qDebug() << "Error socket type";
+            return -1;
             break;
         }
     }
@@ -66,14 +98,60 @@ int TCPSend(int nID, const char* szSendBuf, const char* szDstIP, int nDstPort)
 }
 
 
-QPair<QSharedPointer<QObject>, int> FindSocket(int nId)
+pSocketParam FindSocket(int nId)
 {
     if (g_hashSockets.contains(nId))
     {
-        return g_hashSockets[nId];
+        return g_hashSockets[nId].data();
     }
     else
     {
-        return QPair<QSharedPointer<QObject>, int>();
+        return nullptr;
+    }
+}
+
+
+int TCPConnect(int nID, int nTimeoutMs)
+{
+    pSocketParam pParam = FindSocket(nID);
+    if (pParam->nType != TCP_CLIENT)
+    {
+        qDebug() << "Error in socket type";
+        return -1;
+    }
+    IniConfig config(pParam->szIniPath);
+    QString section = QString().sprintf("TcpClient%d", nID);
+    QTcpSocket* pSocket = (QTcpSocket*)pParam->pObj;
+    pSocket->connectToHost(QHostAddress(config.GetValue(section, "Address", "127.0.0.1").toString()),
+                           config.GetValue(section, "Port", 9999).toInt());
+    if (pSocket->waitForConnected(nTimeoutMs))
+        return 0;
+    else
+    {
+        qDebug() << "connect time timeout";
+        return -1;
+    }
+}
+
+int TCPRecv(int nID, char* szRecvBuf, int nBufLen, int nTimeoutMs)
+{
+    pSocketParam pParam = FindSocket(nID);
+    if (pParam->nType != TCP_CLIENT)
+    {
+        qDebug() << "Error in socket type";
+        return -1;
+    }
+    QTcpSocket* pSocket = (QTcpSocket*)pParam->pObj;
+    if (pSocket->waitForReadyRead(nTimeoutMs))
+    {
+        QByteArray bytes = pSocket->readAll();
+        int nReadSize = (bytes.size() > nBufLen) ? nBufLen : bytes.size();
+        memcpy(szRecvBuf, bytes.data(), nReadSize);
+        return 0;
+    }
+    else
+    {
+        qDebug() << "read time timeout";
+        return -1;
     }
 }
